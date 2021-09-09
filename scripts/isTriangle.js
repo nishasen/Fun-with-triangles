987ccde30e53d1b5d573b6ed63910b4b1c08b296
nishasen <43262505+nishasen@users.noreply.github.com>
2021-09-10 00:12:47 +0530

    Completed

--- scripts/isTriangle.js
+++ scripts/isTriangle.js
@@ -1,10 +1,32 @@
-const angle1 = document.querySelector("#angle1")
-const angle2 = document.querySelector("#angle2")
-const angle3 = document.querySelector("#angle3")
+const angles = document.querySelectorAll(".angles-input")
 const isTriangleButton = document.querySelector("#btn-isTriangle")
 const isTriangleOutput = document.querySelector("#output-is-triangle")
 
+function calculate(a, b, c) {
+    var sum = a + b + c;
+    return sum
+}
+
+function showMessage(message){
+    isTriangleOutput.innerText = message;
+}
+
+function ClearFields() {
+    document.getElementById("angle1").value = "";
+    document.getElementById("angle2").value = "";
+    document.getElementById("angle3").value = "";
+}
+
 isTriangleButton.addEventListener("click", function isTriangleHandler() {
-    console.log(navigator.userAgentData.brands);
-    isTriangleOutput.innerText = angle1 + " " + angle2 + " " + angle3;
+    if(Number(angles[0].value) && Number(angles[1].value) && Number(angles[2].value)) {
+        var sum = calculate(Number(angles[0].value), Number(angles[1].value), Number(angles[2].value)) 
+    if(sum === 180) {
+        showMessage("Yay!! the angles can form a triangle.");
+    } else {
+        showMessage("Nooo!! the angles cannot form a triangle.")
+    }
+    } else {
+        showMessage("Enter all the angles")
+    }
+    ClearFields();
 })
